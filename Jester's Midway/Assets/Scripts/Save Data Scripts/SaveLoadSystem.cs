728faf258d4cecf10a500eb82e9177c30b90e1f6
Alex <ahuberde@my.centennialcollege.ca>
2026-04-02 18:04:27 -0500

    fix save and load on level

--- Jester's Midway/Assets/Scripts/Save Data Scripts/SaveLoadSystem.cs
+++ Jester's Midway/Assets/Scripts/Save Data Scripts/SaveLoadSystem.cs
@@ -7,20 +7,20 @@ public class SaveLoadSystem : PersistentSingleton<SaveLoadSystem>
 
     protected override void Awake()
     {
-        base.Awake(); // Sets up SaveLoadSystem.Instance
-
-        // Handshake with the File Service and your Serializer
+        base.Awake(); 
+        Debug.Log("SAVE PATH: " + Application.persistentDataPath);
+        
         fileDataService = new FileDataService(new JsonSerializer());
     }
 
     public void LoadGame(string gameName)
     {
-        // Try to load the file
+    
         GameData data = fileDataService.Load(gameName);
 
         if (data == null)
         {
-            // If the file doesn't exist yet, create a new one!
+          
             currentData = new GameData();
             currentData.fileName = gameName;
             Debug.Log("No save found. Created new GameData.");
