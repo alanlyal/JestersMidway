f67a5d124bfbfc3e041941920edfe96afea6ae91
Dorian <dorianenglisbee@gmail.ca>
2026-04-02 21:40:02 -0500

    updated the water game to have more visuals and a win/reset condition.

--- Jester's Midway/Assets/Scripts/gunscripts/Water Gun Scripts/WaterTarget.cs
+++ Jester's Midway/Assets/Scripts/gunscripts/Water Gun Scripts/WaterTarget.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class WaterTarget : MonoBehaviour
 {
     public float points = 0f;
+    public float pointsWinThreshold = 500f;
     public float moveTimer = 5f; // moves every n seconds.
+    public GameObject winText;
+    public float gameResetTimer = 10f; // amount of seconds until resetting the score
 
     void Start()
     {
@@ -17,11 +20,29 @@ public class WaterTarget : MonoBehaviour
         {
             points++;
             Destroy(other.gameObject);
+
+            if (points >= pointsWinThreshold)
+            {
+                ActivateWin();
+            }
         }
     }
 
     void MoveTarget()
     {
         transform.position = WaterTargetBounds.Instance.getRandomPosition();
+    }
+
+    void ActivateWin()
+    {
+        winText.SetActive(true);
+
+        Invoke(nameof(DeactivateWin), gameResetTimer);
+    }
+
+    void DeactivateWin()
+    {
+        winText.SetActive(false);
+        points = 0f;
     }
 }
